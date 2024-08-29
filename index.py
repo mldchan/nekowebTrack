53a42efebed153994770543c006eb7fe60cbddbf
mldkyt <mldkyt@proton.me>
2024-08-29 16:34:29 +0200

    Testing Discord webhook

--- index.py
+++ index.py
@@ -21,6 +21,35 @@ logger_file.setLevel(logging.DEBUG)
 logger_file.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(message)s'))
 logger.addHandler(logger_file)
 
+async def send_to_discord(webhook, views, last_visits):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(webhook, json={
+            "embeds": [
+                {
+                    "title": "Nekoweb Tracker",
+                    "description": f"The visitor counter has been updated!",
+                    "color": "#00ff00",
+                    "fields": [
+                        {
+                            "name": "Views",
+                            "value": f"From {last_visits} to {views}!",
+                            "inline": True
+                        },
+                        {
+                            "name": "Difference",
+                            "value": f"{views - last_visits}",
+                            "inline": True
+                        }
+                    ],
+                    "footer": {
+                        "text": "Nekoweb Tracker",
+                        "icon_url": "https://nekoweb.org/favicon.ico"
+                    }
+                }
+            ]
+        }):
+            pass
+
 
 async def main():
     # Let's get the information
@@ -33,6 +62,14 @@ async def main():
 
                 cur = db.cursor()
                 cur.execute("create table if not exists viewshistory(date text, views int);")
+                
+                # Get last visits
+                cur.execute("select * from viewshistory order by date desc limit 1")
+                last_entry = cur.fetchone()
+                if last_entry is not None:
+                    last_visits = last_entry[1]
+                else:
+                    last_visits = 0
 
                 logger.debug('Writing to the database')
                 cur.execute('insert into viewshistory(date, views) values (?, ?)',
@@ -40,6 +77,11 @@ async def main():
 
                 logger.debug('Commiting to the database')
                 db.commit()
+                
+                # Send to Discord webhook
+                if config["webhook"] is not None:
+                    await send_to_discord(config["webhook"], out["views"], last_visits)
+                
                 logger.debug('Finished.')
 
 
