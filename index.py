52219db0506bee134c31f5732f2360b770acbffc
mldchan <mldkyt@proton.me>
2024-09-26 18:31:59 +0200

    Format file

--- index.py
+++ index.py
@@ -23,6 +23,7 @@ logger_file.setLevel(logging.DEBUG)
 logger_file.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(message)s'))
 logger.addHandler(logger_file)
 
+
 async def send_views_to_discord(webhook, views, last_visits):
     if views == last_visits:
         logger.debug('Views are the same, skipping Discord webhook')
@@ -30,78 +31,42 @@ async def send_views_to_discord(webhook, views, last_visits):
     logger.debug('Sending to Discord webhook')
     async with aiohttp.ClientSession() as session:
         logger.debug('Posting to Discord webhook')
-        async with session.post(webhook, json={
-            "embeds": [
-                {
-                    "title": "Nekoweb Tracker",
-                    "description": f"The visitor counter has been updated!",
-                    "color": 0x7FD5EA,
-                    "fields": [
-                        {
-                            "name": "Views",
-                            "value": f"From {last_visits} to {views}!"
-                        },
-                        {
-                            "name": "Difference",
-                            "value": f"{views - last_visits}"
-                        }
-                    ],
-                    "footer": {
-                        "text": "Nekoweb Tracker"
-                    }
-                }
-            ]
-        }) as req:
+        async with session.post(webhook, json={"embeds": [
+            {"title": "Nekoweb Tracker", "description": f"The visitor counter has been updated!", "color": 0x7FD5EA,
+                "fields": [{"name": "Views", "value": f"From {last_visits} to {views}!"},
+                    {"name": "Difference", "value": f"{views - last_visits}"}],
+                "footer": {"text": "Nekoweb Tracker"}}]}) as req:
             if req.status == 204:
                 logger.debug('Discord webhook sent')
             else:
                 logger.error('Failed to send Discord webhook')
-                
+
                 reason = await req.text()
                 logger.error('Discord webhook failed with status %s: %s' % (req.status, reason))
+
 
 async def send_update_to_discord(webhook, updated_at, last_update_at):
     updated_at = datetime.datetime.fromtimestamp(int(updated_at) / 1000)
     last_update_at = datetime.datetime.fromtimestamp(int(last_update_at) / 1000)
-    
+
     if updated_at == last_update_at:
         logger.debug('No updates found, skipping Discord webhook')
         return
     logger.debug('Sending to Discord webhook')
     async with aiohttp.ClientSession() as session:
         logger.debug('Posting to Discord webhook')
-        
+
         from_formatted = last_update_at.strftime('%Y年%m月%d日 %H:%M')
         to_formatted = updated_at.strftime('%Y年%m月%d日 %H:%M')
         diff = updated_at - last_update_at
         diff_formatted = f"{diff.days}日 {diff.seconds // 3600}時間 {(diff.seconds // 60) % 60}分"
-        
-        async with session.post(webhook, json={
-            "embeds": [
-                {
-                    "title": "Nekoweb Tracker",
-                    "description": f"The website has been updated!",
-                    "color": 0x7FD5EA,
-                    "fields": [
-                        {
-                            "name": "Latest Update",
-                            "value": f"{to_formatted}!"
-                        },
-                        {
-                            "name": "Previous Update",
-                            "value": f"{from_formatted}!"
-                        },
-                        {
-                            "name": "Difference",
-                            "value": f"{diff_formatted}"
-                        }
-                    ],
-                    "footer": {
-                        "text": "Nekoweb Tracker"
-                    }
-                }
-            ]
-        }) as req:
+
+        async with session.post(webhook, json={"embeds": [
+            {"title": "Nekoweb Tracker", "description": f"The website has been updated!", "color": 0x7FD5EA,
+                "fields": [{"name": "Latest Update", "value": f"{to_formatted}!"},
+                    {"name": "Previous Update", "value": f"{from_formatted}!"},
+                    {"name": "Difference", "value": f"{diff_formatted}"}],
+                "footer": {"text": "Nekoweb Tracker"}}]}) as req:
             if req.status == 204:
                 logger.debug('Discord webhook sent')
             else:
@@ -122,7 +87,7 @@ async def main():
                 cur = db.cursor()
                 cur.execute("create table if not exists viewshistory(date text, views int);")
                 cur.execute('create table if not exists updatehistory(date text, last_update_date text);')
-                
+
                 # Get last visits
                 cur.execute("select * from viewshistory order by date desc limit 1")
                 last_entry = cur.fetchone()
@@ -137,14 +102,14 @@ async def main():
 
                 logger.debug('Commiting to the database')
                 db.commit()
-                
+
                 # Send to Discord webhook
                 if config["webhook"] is not None:
                     logger.debug('Sending to Discord webhook')
                     await send_views_to_discord(config["webhook"], out["views"], last_visits)
                 else:
                     logger.debug('No webhook provided in config.json, skipping...')
-                    
+
                 # Check if there was an update using updated_at (int milliseconds unix timestamp)
                 if out["updated_at"] is not None:
                     logger.debug('Checking for updates')
@@ -161,7 +126,7 @@ async def main():
                     cur.execute('insert into updatehistory(date, last_update_date) values (?, ?)',
                                 (str(datetime.datetime.now()), out["updated_at"]))
                     db.commit()
-                            
+
                 logger.debug('Finished.')
 
 
